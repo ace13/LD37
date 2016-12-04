b9b5628658c8622454c8e01e5c41769a238879eb
Alexander "Ace" Olofsson <ace@haxalot.com>
2016-12-04 17:46:39 +0100

    Initial commit

---
+++ deps/ASX/README.md
@@ -0,0 +1,3 @@
+These are official Angelscript extensions, taken from the SDK.
+
+If local modifications have been made, then a `.patch` will be provided.
---
+++ game/main.cpp
@@ -0,0 +1,28 @@
+#include <contextmgr/contextmgr.h>
+#include <scriptarray/scriptarray.h>
+#include <scriptdictionary/scriptdictionary.h>
+#include <scriptstdstring/scriptstdstring.h>
+
+#include <chrono>
+
+asUINT getTime()
+{
+	return asUINT(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count());
+}
+
+int main(int argc, char** argv)
+{
+	CContextMgr mgr;
+
+	asIScriptEngine* eng = asCreateScriptEngine();
+	
+	RegisterStdString(eng);
+	RegisterScriptArray(eng, true);
+	RegisterScriptDictionary(eng);
+
+	mgr.SetGetTimeCallback(&getTime);
+	mgr.RegisterCoRoutineSupport(eng);
+	mgr.RegisterThreadSupport(eng);
+
+	return 0;
+}
