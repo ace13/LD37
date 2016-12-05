0e670bba9878f2654f335689e36ce8061c552040
Alexander "Ace" Olofsson <ace@haxalot.com>
2016-12-05 07:54:00 +0100

    Inputs

--- game/InputManager.hpp
+++ game/InputManager.hpp
@@ -1,24 +1,69 @@
 #pragma once
 
+#include <SFML/Window/Joystick.hpp>
+#include <SFML/Window/Keyboard.hpp>
+#include <SFML/Window/Mouse.hpp>
+
 #include <unordered_map>
 
-namespace sf {
-
-}
-
-struct Input
-{
-	float Value;
-
-	operator bool() const;
-
-	void bind();
-};
-
-template<typename T>
 class InputManager
 {
 public:
+	struct Bind
+	{
+		enum Type
+		{
+			Bind_None,
+
+			Bind_Key,
+			Bind_JAxis,
+			Bind_JButton,
+			Bind_MButton,
+			Bind_MWheel
+		} Type;
+
+		union
+		{
+			struct
+			{
+				sf::Keyboard::Key code;
+			} Key;
+
+			struct
+			{
+				unsigned int id;
+				sf::Joystick::Axis axis;
+				bool positive;
+			} JAxis;
+
+			struct
+			{
+				unsigned int id;
+				unsigned int button;
+			} JButton;
+
+			struct
+			{
+				sf::Mouse::Button button;
+			} MButton;
+
+			struct
+			{
+				sf::Mouse::Wheel wheel;
+				bool positive;
+			} MWheel;
+		};
+	};
+
+	struct Input
+	{
+		float Value;
+
+		Bind PBind, SBind, TBind;
+
+		operator bool() const;
+	};
+
 	InputManager();
 	InputManager(const InputManager&);
 	InputManager(InputManager&&);
@@ -28,9 +73,11 @@ public:
 
 	void pushEvent();
 
-	Input& operator[](const T&);
-	const Input& operator[](const T&) const;
+	void addInput(int id);
+
+	Input& operator[](int);
+	const Input& operator[](int) const;
 
 private:
-	std::unordered_map<T, Input> mInputs;
+	std::unordered_map<int, Input> mInputs;
 };
